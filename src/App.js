a35336cf89d63b0795d5bb696a151ecbb769570e
Anthony Martinez <Martinezanthony213@gmail.com>
2019-08-23 15:53:04 -0700

    Adding Api key/Icon/Temps/Location
    Bootstrap installed as well as weather icons
    Api key and services are functional
    Temps and Locations added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Homepage</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'weather-icons/css/weather-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Weather from './app_component/weather.component';
+
+
+// api call api.openweathermap.org/data/2.5/weather?q=London,uk
+const API_Key = "d059fa35b49c92e7bebc7af359e95440";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description: "",
+      error: false
+    };
+    this.getWeather();
+  }
+
+  calCelsius(temp){
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_Key}`
+    );
+
+    const response = await api_call.json();
+
+    console.log(response);
+
+    this.setState({
+      city: response.name,
+      country: response.sys.country,
+      celsius: this.calCelsius(response.main.temp),
+      temp_max: this.calCelsius(response.main.temp_max),
+      temp_min: this.calCelsius(response.main.temp_min),
+      description: response.weather[0].description
+
+
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          temp_celsius={this.state.celsius}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+           />
+      </div>
+    )
+  }
 }
 
 export default App;
