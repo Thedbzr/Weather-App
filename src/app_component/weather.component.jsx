517552b67c2f1e4c928552b8e46b0af9af2a102d
Anthony Martinez <Martinezanthony213@gmail.com>
2019-08-24 13:15:07 -0700

    Full Functionality
    Working Api calls

--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const weather = (props) => {
     return (
-        <div className="container">
+        <div className="container pt-4 text-light">
             <div className="cards">
-                <h1>{props.city},{props.country}</h1>
+                <h1>{props.city}</h1>
                 <h5 className="py-4">
-                    <i className="wi wi-day-sunny display-1" />
+                    <i className={`wi ${props.weatherIcon} display-1`} />
                 </h5>
-                <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+                {props.temp_celcius ? (<h1 className="py-2">{props.temp_celsius}&deg;</h1>) : null}
                 {/** Show max and min temp */}
-                {minmaxTemp(props.temp_min,props.temp_max)}
+                {minmaxTemp(props.temp_min, props.temp_max)}
 
                 <h4 className="py-3">{props.description}</h4>
             </div>
@@ -18,14 +18,15 @@ const weather = (props) => {
     );
 }
 
-function minmaxTemp(min,max){
-    return(
-        <h3>
-            <span className="px-4">{min}&deg;</span>
-            <span className="px-4">{max}&deg;</span>
-        </h3>
-    )
+function minmaxTemp(min, max) {
+    if (min && max)
+        return (
+            <h3>
+                <span className="px-4">{min}&deg;</span>
+                <span className="px-4">{max}&deg;</span>
+            </h3>
+        );
 
-}
+};
 
 export default weather;
