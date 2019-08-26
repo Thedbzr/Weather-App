9a3c5cf06e435be0129655159ee11dae219841b6
Benjamin Chacko <benjaminchacko@outlook.com>
2019-08-26 12:00:44 -0500

    Updated navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import '../assets/css/navbar.css'
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "../assets/css/navbar.css";
 
 const NavBar = () => {
-    return (
-        <React.Fragment>
-            <h1>NavBar Component</h1>
-        </React.Fragment>
-      );
-}
- 
-export default NavBar
+  return (
+    <React.Fragment>
+      <Navbar bg="light" expand="lg">
+      <Navbar.Brand id="logo" href="/">Weather App</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/cities">Cities Database</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </React.Fragment>
+  );
+};
+
+export default NavBar;
